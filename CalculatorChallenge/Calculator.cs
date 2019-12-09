5d97086c4a53723b7fb92df38fa07ead22fc3abc
Randall Kelley <rkelley@restaurant365.com>
2019-12-09 13:12:17 -0800

    Modifications to contend with different OS newline syntax

--- CalculatorChallenge/Calculator.cs
+++ CalculatorChallenge/Calculator.cs
@@ -6,23 +6,20 @@ namespace CalculatorChallenge
 {
     class Calculator
     {
+        private static string[] NewLines = new string[] { Environment.NewLine, "\\n", "\n" };
+        private static string NewLine;
         public static void Main(string[] args)
         {
             Console.WriteLine("Enter a set of numbers to be added:");
-
             string numbersInput = Console.ReadLine();
-
             int answer = AddNumbers(numbersInput);
-
             Console.WriteLine("The answer is " + answer.ToString());
         }
 
         public static int AddNumbers(string userInput)
         {
             int answer = 0;
-
             List<string> negativeNumberList = new List<string>();
-
             string[] numbersList = GetNumberList(userInput);
 
             foreach (var item in numbersList)
@@ -45,7 +42,6 @@ namespace CalculatorChallenge
             if (negativeNumberList.Count > 0)
             {
                 string invalidNumbers = string.Join(", ", negativeNumberList.ToArray());
-
                 throw new Exception("Invalid negative numbers were input: " + invalidNumbers);
             }
 
@@ -56,27 +52,20 @@ namespace CalculatorChallenge
         {
             string numberBlock = userInput;
 
-            string[] delimiters = { ",", Environment.NewLine, "\\n" };
+            string[] delimiters = { ","};
+            delimiters = CombineTwoArrays(delimiters, NewLines);
 
             if (userInput.Length > 2)
             {
                 string firstTwoCharacters = userInput.Substring(0, 2);
-                bool containsNewLine = userInput.Contains("\\n") ||
-                    userInput.Contains(Environment.NewLine);
-
+                NewLine = DetermineNewLine(userInput);
+                bool containsNewLine = !string.IsNullOrEmpty(NewLine);
+                
                 if (firstTwoCharacters.Equals("//") && containsNewLine)
                 {
                     var userDefinedDelimiterList = ReturnDelimiters(userInput);
-
-                    var tempList = new List<string>();
-                    tempList.AddRange(delimiters);
-                    tempList.AddRange(userDefinedDelimiterList);
-                    delimiters = tempList.ToArray();
-
-                    string newLine = userInput.Contains("\\n") ? "\\n" : Environment.NewLine;
-
-                    int startOfNumbers = userInput.IndexOf(newLine, StringComparison.CurrentCulture) + newLine.Length;
-
+                    delimiters = CombineTwoArrays(delimiters, userDefinedDelimiterList);
+                    int startOfNumbers = userInput.IndexOf(NewLine, StringComparison.CurrentCulture) + NewLine.Length;
                     numberBlock = userInput.Substring(startOfNumbers, userInput.Length - startOfNumbers);
                 }
             }
@@ -84,17 +73,35 @@ namespace CalculatorChallenge
             return numberBlock.Split(delimiters, StringSplitOptions.None);
         }
 
+        private static string[] CombineTwoArrays(string[] array1, string[] array2)
+        {
+            var tempList = new List<string>();
+            tempList.AddRange(array1);
+            tempList.AddRange(array2);
+            return tempList.ToArray();
+        }
+
         private static string[] ReturnDelimiters(string numberString)
         {
-            string newLine = numberString.Contains("\\n") ? "\\n" : Environment.NewLine;
-
-            int endOfDelimiters = numberString.IndexOf(newLine, StringComparison.CurrentCulture);
-
+            int endOfDelimiters = numberString.IndexOf(NewLine, StringComparison.CurrentCulture);
             string delimiterBlock = numberString.Substring(2, endOfDelimiters - 2);
-
             string regexSplitter = @"\[([^\]]*)\]";
-
             return Regex.Split(delimiterBlock, regexSplitter);
+        }
+
+        private static string DetermineNewLine(string input)
+        {
+            string newLine = null;
+            foreach (string item in NewLines)
+            {
+                if (input.Contains(item))
+                {
+                    newLine = item;
+                    break;
+                }
+            }
+
+            return newLine;
         }
     }
 }
