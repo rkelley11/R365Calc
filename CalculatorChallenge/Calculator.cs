72fbf0e3479d968bb1962e202c6825d66d42ef14
Randall Kelley <randall.kelley@autoalert.com>
2019-12-08 23:11:10 -0800

    Requirement 1: Support 2 numbers only with comman delimiter

---
+++ CalculatorChallenge/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CalculatorChallenge
+{
+    class Calculator
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a set of numbers to be added:");
+
+            string numbersInput = Console.ReadLine();
+
+            int answer = AddNumbers(numbersInput);
+
+            Console.WriteLine("The answer is " + answer.ToString());
+        }
+
+        public static int AddNumbers(string userInput)
+        {
+            int answer = 0;
+
+            string[] numbersList = GetNumberList(userInput);
+
+            if (numbersList.Length > 2) throw new Exception("More than 2 numbers were input");
+
+            foreach (var item in numbersList)
+            {
+                int.TryParse(item, out int itemInteger);
+
+                answer += itemInteger;
+            }
+
+            return answer;
+        }
+
+        private static string[] GetNumberList(string userInput)
+        {
+            string numberBlock = userInput;
+
+            string[] delimiters = { ","};
+
+            return numberBlock.Split(delimiters, StringSplitOptions.None);
+        }
+    }
+}
---
+++ CalculatorChallenge/CalculatorTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorChallenge
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void AcceptsSingleInteger()
+        {
+            int answer = Calculator.AddNumbers("20");
+
+            Assert.IsTrue(answer == 20);
+        }
+
+        [TestMethod]
+        public void AcceptsTwoPositiveIntegers()
+        {
+            int answer = Calculator.AddNumbers("1,5000");
+
+            Assert.IsTrue(answer == 5001);
+        }
+
+        [TestMethod]
+        public void AcceptsSinglePositiveSingleNegativeIntegers()
+        {
+            int answer = Calculator.AddNumbers("4, -3");
+
+            Assert.IsTrue(answer == 1);
+        }
+
+        [TestMethod]
+        public void AcceptsEmptyInput()
+        {
+            int answer = Calculator.AddNumbers("");
+
+            Assert.IsTrue(answer == 0);
+        }
+
+        [TestMethod]
+        public void ConvertsCharactersToZero()
+        {
+            int answer = Calculator.AddNumbers("5,tytyt");
+
+            Assert.IsTrue(answer == 5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "More than 2 numbers were input")]
+        public void MoreThanTwoNumbersThrowsException()
+        {
+            int answer = Calculator.AddNumbers("5,10, 15");
+        }
+    }
+}
